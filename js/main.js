53a693f76d2f8e3660cc3eb5526120910e7e2ea4
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-06-23 21:37:19 +0500

    faq

--- js/main.js
+++ js/main.js
@@ -9,3 +9,28 @@ elsIndexServicesLinks.forEach(function (link) {
     link.closest('.services-tabs__item').classList.add('services-tabs__item--active');
   })
 })
+
+
+let  elsGiveQuestionLink = document.querySelector('.site-header__link');
+let  elsFaqSection  = document.querySelector('.modal-faq');
+let  elsFaqButton  = document.querySelector('.modal-faq__cancel-button');
+
+ elsGiveQuestionLink.addEventListener('click',function (evt) {
+   evt.preventDefault();
+  elsFaqSection.classList.add('modal-faq--active');
+
+})
+
+elsFaqButton.addEventListener('click',function () {
+  elsFaqSection.classList.remove('modal-faq--active');
+})
+
+
+elsqaItem = document.querySelectorAll('.qa');
+elsqaToggler = document.querySelectorAll('.qa-question__toggler');
+
+elsqaToggler.forEach(function (toggler) {
+  toggler.addEventListener('click',function () {
+      toggler.closest('.qa').classList.toggle('qa--active');
+  })
+})
