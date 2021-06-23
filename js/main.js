7e524d0bf05a55f43a6db8644dafbbf61c0291d3
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-06-23 22:41:59 +0500

    index bugs fixed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ elsIndexServicesLinks = document.querySelectorAll('.services-tabs__link');
 elsIndexServicesItems = document.querySelectorAll('.services-tabs__item');
 
 elsIndexServicesLinks.forEach(function (link) {
-  link.addEventListener('click', function () {
+  link.addEventListener('click', function (evt) {
+    evt.preventDefault();
     elsIndexServicesItems.forEach( function (item) {
       item.classList.remove('services-tabs__item--active');
     })
@@ -32,5 +33,23 @@ elsqaToggler = document.querySelectorAll('.qa-question__toggler');
 elsqaToggler.forEach(function (toggler) {
   toggler.addEventListener('click',function () {
       toggler.closest('.qa').classList.toggle('qa--active');
+  })
+})
+
+
+
+// article
+
+elsArticleITem  = document.querySelectorAll('.article-card');
+elsArticleLink = document.querySelectorAll('.article-card__link');
+
+elsArticleLink.forEach(function (link) {
+  link.addEventListener('click',function (evt) {
+    evt.preventDefault();
+    elsArticleITem.forEach(function (item) {
+      item.classList.remove('article-card__open');
+
+    })
+    link.closest('.article-card').classList.add('article-card__open');
   })
 })
